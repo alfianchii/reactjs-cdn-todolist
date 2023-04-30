0b77a033c4e5b62bae438ab4fec1953e5c7be552
alfianchii <pianaee379@gmail.com>
2023-04-30 17:42:20 +0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+function App() {
+	return (
+		<>
+			<h1>Simple Todo List</h1>
+		</>
+	);
+}
+
+root.render(<App />);
