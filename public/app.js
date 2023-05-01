11a0f6a5d58156e3974a209cc653968cfe942003
alfianchii <pianaee379@gmail.com>
2023-05-01 07:43:35 +0700

    feat(ui): bootstrap

--- public/app.js
+++ public/app.js
@@ -78,34 +78,71 @@ function App() {
     // return cancelEditHandler();
   }
 
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple Todo List"), message && /*#__PURE__*/React.createElement("div", {
-    style: {
-      color: "red"
-    }
-  }, /*#__PURE__*/React.createElement("i", null, message)), /*#__PURE__*/React.createElement("form", {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "row"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "col"
+  }, /*#__PURE__*/React.createElement("h1", {
+    className: "text-center mb-4"
+  }, "Simple Todo-list Application"), message && /*#__PURE__*/React.createElement("div", {
+    className: "alert alert-danger",
+    role: "alert"
+  }, message)), /*#__PURE__*/React.createElement("form", {
+    className: "mb-3",
     onSubmit: saveTodoHandler
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "input-group mb-3"
   }, /*#__PURE__*/React.createElement("input", {
     autoFocus: true,
     type: "text",
+    className: "form-control",
+    id: "nama-aktifitas",
     placeholder: "Nama aktifitas ...",
     value: activity,
     onChange: event => setActivity(event.target.value)
   }), /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-outline-primary",
     type: "submit"
   }, edit.id ? "Simpan" : "Tambah"), edit.id && /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-outline-danger me-2",
     onClick: cancelEditHandler
-  }, "Batal edit")), todos.length > 0 ? /*#__PURE__*/React.createElement("ul", null, todos.map(todo => /*#__PURE__*/React.createElement("li", {
+  }, "Batal edit"))), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement("table", {
+    className: "table table-striped"
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "#"), /*#__PURE__*/React.createElement("th", null, "Aktifitas"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Aksi"))), /*#__PURE__*/React.createElement("tbody", null, todos.length > 0 ? /*#__PURE__*/React.createElement(React.Fragment, null, todos.map((todo, index) => /*#__PURE__*/React.createElement("tr", {
     key: todo.id
+  }, /*#__PURE__*/React.createElement("td", {
+    className: "align-middle"
+  }, index + 1), /*#__PURE__*/React.createElement("td", {
+    className: "align-middle"
   }, /*#__PURE__*/React.createElement("input", {
+    className: "form-check-input me-2",
     type: "checkbox",
     checked: todo.done,
+    id: todo.id,
     onChange: doneTodoHandler.bind(this, todo)
-  }), todo.activity, " (", todo.done ? "Selesai" : "Belum selesai", ")", /*#__PURE__*/React.createElement("button", {
+  }), /*#__PURE__*/React.createElement("label", {
+    className: "form-check-label",
+    htmlFor: todo.id
+  }, todo.activity)), /*#__PURE__*/React.createElement("td", {
+    className: "align-middle"
+  }, todo.done ? /*#__PURE__*/React.createElement("span", {
+    className: "badge text-bg-success"
+  }, "Selesai") : /*#__PURE__*/React.createElement("span", {
+    className: "badge text-bg-danger"
+  }, "Belum selesai")), /*#__PURE__*/React.createElement("td", {
+    className: "align-middle"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-success me-1",
     type: "submit",
     onClick: editTodoHandler.bind(this, todo)
   }, "Edit"), /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-danger me-1",
     type: "submit",
     onClick: removeTodoHandler.bind(this, todo.id)
-  }, "Hapus")))) : /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("i", null, "Tidak ada aktifitas.")));
+  }, "Hapus"))))) : /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    colSpan: "4"
+  }, /*#__PURE__*/React.createElement("p", {
+    className: "text-center mt-3"
+  }, "Tidak ada aktifitas."))))));
 }
 root.render( /*#__PURE__*/React.createElement(App, null));
--- src/app.js
+++ src/app.js
@@ -92,42 +92,76 @@ function App() {
 	}
 
 	return (
-		<>
-			<h1>Simple Todo List</h1>
+		<div className="row">
+			<div className="col">
+				<h1 className="text-center mb-4">Simple Todo-list Application</h1>
 
-			{message && (
-				<div style={{ color: "red" }}>
-					<i>{message}</i>
+				{message && (
+					<div className="alert alert-danger" role="alert">
+						{message}
+					</div>
+				)}
+			</div>
+
+			<form className="mb-3" onSubmit={saveTodoHandler}>
+				<div className="input-group mb-3">
+					<input autoFocus type="text" className="form-control" id="nama-aktifitas" placeholder="Nama aktifitas ..." value={activity} onChange={(event) => setActivity(event.target.value)} />
+					<button className="btn btn-outline-primary" type="submit">
+						{edit.id ? "Simpan" : "Tambah"}
+					</button>
+					{edit.id && (
+						<button className="btn btn-outline-danger me-2" onClick={cancelEditHandler}>
+							Batal edit
+						</button>
+					)}
 				</div>
-			)}
-
-			<form onSubmit={saveTodoHandler}>
-				<input autoFocus type="text" placeholder="Nama aktifitas ..." value={activity} onChange={(event) => setActivity(event.target.value)} />
-				<button type="submit">{edit.id ? "Simpan" : "Tambah"}</button>
-				{edit.id && <button onClick={cancelEditHandler}>Batal edit</button>}
 			</form>
 
-			{todos.length > 0 ? (
-				<ul>
-					{todos.map((todo) => (
-						<li key={todo.id}>
-							<input type="checkbox" checked={todo.done} onChange={doneTodoHandler.bind(this, todo)} />
-							{todo.activity} ({todo.done ? "Selesai" : "Belum selesai"})
-							<button type="submit" onClick={editTodoHandler.bind(this, todo)}>
-								Edit
-							</button>
-							<button type="submit" onClick={removeTodoHandler.bind(this, todo.id)}>
-								Hapus
-							</button>
-						</li>
-					))}
-				</ul>
-			) : (
-				<p>
-					<i>Tidak ada aktifitas.</i>
-				</p>
-			)}
-		</>
+			<hr />
+
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Aktifitas</th>
+						<th>Status</th>
+						<th>Aksi</th>
+					</tr>
+				</thead>
+				<tbody>
+					{todos.length > 0 ? (
+						<>
+							{todos.map((todo, index) => (
+								<tr key={todo.id}>
+									<td className="align-middle">{index + 1}</td>
+									<td className="align-middle">
+										<input className="form-check-input me-2" type="checkbox" checked={todo.done} id={todo.id} onChange={doneTodoHandler.bind(this, todo)} />
+										<label className="form-check-label" htmlFor={todo.id}>
+											{todo.activity}
+										</label>
+									</td>
+									<td className="align-middle">{todo.done ? <span className="badge text-bg-success">Selesai</span> : <span className="badge text-bg-danger">Belum selesai</span>}</td>
+									<td className="align-middle">
+										<button className="btn btn-success me-1" type="submit" onClick={editTodoHandler.bind(this, todo)}>
+											Edit
+										</button>
+										<button className="btn btn-danger me-1" type="submit" onClick={removeTodoHandler.bind(this, todo.id)}>
+											Hapus
+										</button>
+									</td>
+								</tr>
+							))}
+						</>
+					) : (
+						<tr>
+							<td colSpan="4">
+								<p className="text-center mt-3">Tidak ada aktifitas.</p>
+							</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
+		</div>
 	);
 }
 
